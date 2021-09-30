bd220f7ba327fc103f2056b8f8593d2c7512b7b6
DevApoorv <singhalapoorv@rediff.com>
2021-09-30 23:18:01 +0530

    Update CS

--- central_system.py
+++ central_system.py
@@ -1,5 +1,6 @@
 import asyncio
 import logging
+from pymongo import MongoClient
 from datetime import datetime
 
 try:
@@ -13,82 +14,169 @@ except ModuleNotFoundError:
     sys.exit(1)
 
 from ocpp.routing import on
-from ocpp.v201 import ChargePoint as cp
-from ocpp.v201 import call_result
+from ocpp.v16 import ChargePoint as cp
+from ocpp.v16 import call_result
+from ocpp.v16.enums import Action, RegistrationStatus
 
 logging.basicConfig(level=logging.INFO)
 
+client = MongoClient('localhost', 27017)
+mydatabase = client['ocppdb'] 
 
-class ChargePoint(cp):
-    @on('BootNotification')
-    def on_boot_notification(self, charging_station, reason, **kwargs):
+class MyChargePoint(cp):
+
+    @on(Action.Authorize)
+    def authorize(self, id_tag):
+        record_count = 0
+        result = mydatabase.vehicle.find({"idTag": id_tag})
+        result_iter = {}
+        response = {"title": "AuthorizeResponse", "type": "object"}
+        for x in result:
+            record_count += 1
+            result_iter = x
+        if(record_count != 0):
+            response["properties"] = {}
+            response["properties"]["idTagInfo"] = {"status": result_iter["status"], "expiryDate": result_iter["expiryDate"], "parentIdTag": result_iter["parentIdTag"]}
+        else:
+            response["properties"] = {}
+            response["properties"]["idTagInfo"] = {"status": "Invalid"}
+        
+        return response
+    # [2,
+    #          "19223201",
+    #          "BootNotification",
+    #          {
+    #              "chargePointVendor":"Power Grid",
+    #              "chargePointModel":"Model-1 Optimus"
+
+    #          }
+    #         ]
+
+    @on(Action.BootNotification)
+    def on_boot_notitication(self, charge_point_vendor, charge_point_model,**kwargs):
         return call_result.BootNotificationPayload(
             current_time=datetime.utcnow().isoformat(),
             interval=10,
-            status='Accepted'
+            status=RegistrationStatus.accepted
         )
 
-    @on('Heartbeat')
+    # [2,
+    #          "19223202",
+    #          "Heartbeat",
+    #          {}
+    #         ]
+
+    @on(Action.Heartbeat)
     def on_heartbeat(self):
         print('Got a Heartbeat!')
         return call_result.HeartbeatPayload(
             current_time=datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S') + "Z"
         )
 
+    # Method used by charge point 
+    @on(Action.MeterValues)
+    def meter_values(self, connector_id, meter_value, transaction_id):
+        return call_result.MeterValuesPayload(
+            connectorId=connector_id,
+            meterValue=meter_value,
+            transactionId=transaction_id
+        )
+
+    # Change availability of connector type- Operative, Inoperative
+    @on(Action.ChangeAvailability)
+    def on_change_availability(self, connector_id, type):
+        return call_result.ChangeAvailabilityPayload(
+            status=RegistrationStatus.accepted
+        )
+
+    #     [
+    # 2,
+    # "26",
+    # "StartTransaction",
+    # {
+    # "connectorId": 1,
+    # "idTag": "dummyToken",
+    # "meterStart": 0,
+    # "timestamp": "2017-03-08T14:22:27Z"
+    # }
+    # ]
+
+    @on(Action.StartTransaction)
+    def on_start_transaction(self, connector_id, id_tag, meter_start,timestamp):
+        obj={"status": "Accepted"}
+        return call_result.StartTransactionPayload(
+            transaction_id=1111,
+            id_tag_info=obj
+        )
+
+        #     [
+        # 2,
+        # "138ffd31-9245-4c63-9185-4036067dffb9",
+        # "RemoteStartTransaction",
+        # {
+        # "connectorId": 1,
+        # "idTag": "dummyToken",
+        # "chargingProfile": {
+        # "chargingProfileId": 2,
+        # "transactionId": 123,
+        # "stackLevel": 1,
+        # "chargingProfilePurpose": "TxProfile",
+        # "chargingProfileKind": "Relative",
+        # "chargingSchedule": {
+        # "duration": 5,
+        # "chargingRateUnit": "W",
+        # "chargingSchedulePeriod": [
+        # {
+        # "startPeriod": 0,
+        # "limit": 1000,
+        # "numberPhases": 3
+        # }
+        # ]
+        # }
+        # }
+        # }
+        # ]
+    @on('RemoteStartTransaction')
+    def remote_start_transaction(self, connector_id,id_tag, charging_profile):
+        return call_result.RemoteStartTransactionPayload(
+            status=RegistrationStatus.accepted
+        )
+
+    # [
+    # 2,
+    # "0a8a0fab-7775-4f72-9f4b-ed5bd5a4bed2",
+    # "RemoteStopTransaction",
+    # {
+    # "transactionId": 123
+    # }
+    # ]
+    @on('RemoteStopTransaction')
+    def remote_stop_transaction(self, transaction_id):
+        return call_result.RemoteStopTransactionPayload(
+            status=RegistrationStatus.accepted
+        )
+
 
 async def on_connect(websocket, path):
-    """ For every new charge point that connects, create a ChargePoint
-    instance and start listening for messages.
+    """ For every new charge point that connects, create a ChargePoint instance
+    and start listening for messages.
+
     """
-    try:
-        requested_protocols = websocket.request_headers[
-            'Sec-WebSocket-Protocol']
-    except KeyError:
-        logging.info("Client hasn't requested any Subprotocol. "
-                     "Closing Connection")
-        return await websocket.close()
-        logging.error(
-            "Client hasn't requested any Subprotocol. Closing Connection"
-        )
-        return await websocket.close()
-    if websocket.subprotocol:
-        logging.info("Protocols Matched: %s", websocket.subprotocol)
-    else:
-        # In the websockets lib if no subprotocols are supported by the
-        # client and the server, it proceeds without a subprotocol,
-        # so we have to manually close the connection.
-        logging.warning('Protocols Mismatched | Expected Subprotocols: %s,'
-                        ' but client supports %s | Closing connection',
-                        websocket.available_subprotocols,
-                        requested_protocols)
-        return await websocket.close()
-
     charge_point_id = path.strip('/')
-    charge_point = ChargePoint(charge_point_id, websocket)
+    cp = MyChargePoint(charge_point_id, websocket)
 
-    await charge_point.start()
-
+    await cp.start()
 
 async def main():
-    #  deepcode ignore BindToAllNetworkInterfaces: <Example Purposes>
-    server = await websockets.serve(
-        on_connect,
-	'https://sleepy-river-71702.herokuapp.com',
-        subprotocols=['ocpp2.0.1']
-    )
+   server = await websockets.serve(
+      on_connect,
+      'localhost',
+      9000,
+      subprotocols=['ocpp2.0.1']
+   )
 
-    logging.info("Server Started listening to new connections...")
-    await server.wait_closed()
+   await server.wait_closed()
 
 
 if __name__ == '__main__':
-    try:
-        # asyncio.run() is used when running this example with Python 3.7 and
-        # higher.
-        asyncio.run(main())
-    except AttributeError:
-        # For Python 3.6 a bit more code is required to run the main() task on
-        # an event loop.
-        loop = asyncio.get_event_loop()
-        loop.run_until_complete(main())
-        loop.close()
+   asyncio.run(main())
